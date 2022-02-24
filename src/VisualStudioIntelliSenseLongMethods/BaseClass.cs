2867b30b43e408e7b19a853d09c8cf8fe3859485
ayhrgr <ayhrgr@gmail.com>
2022-02-24 23:29:21 +0900

    add csharp sample project.

---
+++ src/VisualStudioIntelliSenseLongMethods/BaseClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xxxxx.VisualStudioIntelliSenseLongMethods.Test.Response;
+
+namespace VisualStudioIntelliSenseLongMethods
+{
+    internal class BaseClass
+    {
+        protected virtual VisualStudioIntelliSenseLongMethodsTest_ResponseData? 
+            ClientProcessDataSouceAndPrintingTestMethod(
+            xxxxx.VisualStudioIntelliSenseLongMethods.Test.Request.VisualStudioIntelliSenseLongMethodsTest_RequestData requestData01,
+            xxxxx.VisualStudioIntelliSenseLongMethods.Test.Request.VisualStudioIntelliSenseLongMethodsTest_RequestData requestData02,
+            xxxxx.VisualStudioIntelliSenseLongMethods.Test.Request.VisualStudioIntelliSenseLongMethodsTest_RequestData requestData03
+            )
+        {
+            return null;
+        }
+    }
+}
---
+++ src/VisualStudioIntelliSenseLongMethods/ClientClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xxxxx.VisualStudioIntelliSenseLongMethods.Test.Request;
+using xxxxx.VisualStudioIntelliSenseLongMethods.Test.Response;
+
+namespace VisualStudioIntelliSenseLongMethods
+{
+    internal class ClientClass: BaseClass
+    {
+        override 
+    }
+}
